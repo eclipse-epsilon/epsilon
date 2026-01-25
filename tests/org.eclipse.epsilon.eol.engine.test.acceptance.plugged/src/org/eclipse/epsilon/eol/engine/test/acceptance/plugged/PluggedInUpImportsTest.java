63a76d7e9db2f83b8812cb9ef9c860e83d3def53
Antonio Garcia-Dominguez <agarcdomi@gmail.com>
2026-01-25 17:02:01 +0000

    Add plugged-in test and generic URI support

---
+++ tests/org.eclipse.epsilon.eol.engine.test.acceptance.plugged/src/org/eclipse/epsilon/eol/engine/test/acceptance/plugged/PluggedInUpImportsTest.java
@@ -0,0 +1,81 @@
+package org.eclipse.epsilon.eol.engine.test.acceptance.plugged;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Collections;
+
+import org.apache.commons.io.FileUtils;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.epsilon.eol.EolModule;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class PluggedInUpImportsTest {
+
+	private static final String PROJECT_NAME = "pluggedin.resources";
+	private static final String FOLDER_NAME = "resources";
+
+	@BeforeClass
+	public static void setUpProject() throws Exception {
+		IProgressMonitor monitor = new NullProgressMonitor();
+		
+		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+		IProject project = root.getProject(PROJECT_NAME);
+		if (!project.exists()) {
+			project.create(monitor);
+		}
+		project.open(monitor);
+
+		File sourceDir = new File(
+			new File("../org.eclipse.epsilon.eol.engine.test.acceptance.plugged"),
+			FOLDER_NAME
+		);
+		File targetDir = new File(project.getLocation().toFile(), sourceDir.getName());
+		if (targetDir.exists()) {
+			FileUtils.deleteDirectory(targetDir);
+		}
+		FileUtils.copyDirectory(sourceDir, targetDir);
+
+		project.refreshLocal(IResource.DEPTH_INFINITE, monitor);
+	}
+
+	@Test
+	public void directRoot() throws Exception {
+		EolModule module = parse("level1b/directRoot.eol");
+		assertEquals("i am root", module.execute());
+	}
+
+	@Test
+	public void level1OnlyName() throws Exception {
+		EolModule module = parse("level1b/useLevel1.eol");
+		assertEquals("level 1", module.execute());
+	}
+
+	@Test
+	public void level2OnlyName() throws Exception {
+		EolModule module = parse("level1b/useLevel2.eol");
+		assertEquals("level 2", module.execute());
+	}
+
+	protected EolModule parse(String path) throws Exception {
+		EolModule module = new EolModule();
+		module.parse(computePlatformURI(path));
+		assertEquals("There should not be any parse problems",
+			Collections.emptyList(), module.getParseProblems());
+		return module;
+	}
+
+	protected URI computePlatformURI(String path) throws URISyntaxException {
+		return new URI(String.format(
+			"platform:/resource/%s/%s/upImports/%s",
+			PROJECT_NAME, FOLDER_NAME, path));
+	}
+}
