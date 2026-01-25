6178d1da0a3e72ca9ebb869ae710649bf96beeac
Antonio Garcia-Dominguez <agarcdomi@gmail.com>
2026-01-25 17:02:01 +0000

    Add plugged-in test and generic URI support

--- plugins/org.eclipse.epsilon.common/src/org/eclipse/epsilon/common/util/UriUtil.java
+++ plugins/org.eclipse.epsilon.common/src/org/eclipse/epsilon/common/util/UriUtil.java
@@ -11,6 +11,7 @@ package org.eclipse.epsilon.common.util;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.file.FileSystem;
@@ -18,6 +19,7 @@ import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
@@ -91,58 +93,87 @@ public class UriUtil {
 		}
 
 		for (URI rel : relativeTo) {
-			FileSystem zipfs = null;
 			try {
 				if ("jar".equals(rel.getScheme())) {
-					/*
-					 * We need to open a file system for the zip file before accessing it with Path,
-					 * as this is not done automatically.
-					 */
-					zipfs = FileSystems.newFileSystem(rel, Collections.emptyMap());
-				} else if (!"file".equals(rel.getScheme())) {
-					// up: URIs are only supported from modules with jar: and file: URIs for now
-					continue;
-				}
-
-				Path relPath = Paths.get(rel).toAbsolutePath();
-				Optional<Path> subPath = firstAncestorDirectoryNamed(relPath, uri.getAuthority());
-				if (!subPath.isPresent()) {
-					continue;
-				}
-
-				// Look for files whose path ends with the desired suffix
-				List<Path> candidates = Files.walk(subPath.get())
-					.filter(p -> p.toFile().isFile())
-					.filter(p -> pathEndsWith(p, uriPath))
-					.collect(Collectors.toList());
-
-				if (candidates.size() > 1) {
-					throw new URISyntaxException(uri.toString(), String.format(
-						"Ambiguous - possible options:\n%s",
-						String.join(
-							System.lineSeparator(),
-							candidates.stream()
-								.map(c -> "- " + c.toString())
-								.collect(Collectors.toList())
-						)
-					));
-				} else if (candidates.size() == 1) {
-					return candidates.get(0).toUri();
+					try (FileSystem zipfs = FileSystems.newFileSystem(rel, Collections.emptyMap())) {
+						/*
+						 * We need to open a file system for the zip file before accessing it with Path,
+						 * as this is not done automatically.
+						 */
+						Optional<URI> resolved = resolveUpViaPaths(uri, rel);
+						if (resolved.isPresent()) {
+							return resolved.get();
+						}
+					}
+				} else if ("file".equals(rel.getScheme())) {
+					Optional<URI> resolved = resolveUpViaPaths(uri, rel);
+					if (resolved.isPresent()) {
+						return resolved.get();
+					}
+				} else if (rel.isAbsolute() && !rel.isOpaque()) {
+					Optional<URI> resolved = resolveUpViaSegments(uri, rel);
+					if (resolved.isPresent()) {
+						return resolved.get();
+					}
 				}
 			} catch (IOException ex) {
 				ex.printStackTrace();
-			} finally {
-				if (zipfs != null) {
-					try {
-						zipfs.close();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-				}
 			}
 		}
 
 		return uri;
+	}
+
+	protected static Optional<URI> resolveUpViaSegments(URI uri, URI rel) throws URISyntaxException, IOException {
+		String[] segments = rel.getPath().split("/");
+
+		for (int iSegment = segments.length - 1; iSegment >= 0; --iSegment) {
+			if (uri.getAuthority().equals(segments[iSegment])) {
+				List<String> subSegments = Arrays.asList(segments).subList(0, iSegment+1);
+				String pathToSegment = String.join("/", subSegments);
+				URI resolved = new URI(rel.getScheme(), rel.getAuthority(),
+					pathToSegment + uri.getPath(), null);
+
+				// Try to open the URL - if we succeed, resolve it
+				try (InputStream is = resolved.toURL().openStream()) {
+					return Optional.of(resolved);
+				}
+			}
+		}
+
+		return Optional.empty();
+	}
+
+	protected static Optional<URI> resolveUpViaPaths(URI uri, URI sourceModuleURI)
+			throws URISyntaxException, IOException
+	{
+		Path uriPath = Paths.get(uri.getPath());
+		Path relPath = Paths.get(sourceModuleURI).toAbsolutePath();
+		Optional<Path> subPath = firstAncestorDirectoryNamed(relPath, uri.getAuthority());
+		if (!subPath.isPresent()) {
+			return Optional.empty();
+		}
+
+		// Look for files whose path ends with the desired suffix
+		List<Path> candidates = Files.walk(subPath.get())
+			.filter(p -> p.toFile().isFile())
+			.filter(p -> pathEndsWith(p, uriPath))
+			.collect(Collectors.toList());
+
+		if (candidates.size() > 1) {
+			throw new URISyntaxException(uri.toString(), String.format(
+				"Ambiguous - possible options:\n%s",
+				String.join(
+					System.lineSeparator(),
+					candidates.stream()
+						.map(c -> "- " + c.toString())
+						.collect(Collectors.toList())
+				)
+			));
+		} else if (candidates.size() == 1) {
+			return Optional.of(candidates.get(0).toUri());
+		}
+		return Optional.empty();
 	}
 
 	/**
